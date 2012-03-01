fc2f5ef1190f91de5b18dc9df9f30cdf6020ffe4
piat.jonathan <piat.jonathan@a556c50f-5b6a-0615-30e4-5cdc83f93d05>
2012-03-01 14:08:38 +0000

    simulation

--- hdl/fifo_64x8.vhd
+++ hdl/fifo_64x8.vhd
@@ -17,8 +17,10 @@ end fifo_64x8;
 architecture systemc of fifo_64x8 is
  
 	signal wr_addr, rd_addr, byte_count, ram_addr : std_logic_vector(5 downto 0) ;
-	signal fullt, emptyt : std_logic ;
+	signal fullt, emptyt, ready, ram_en : std_logic ;
 	begin
+	
+	ram_en <=  rd OR wr ;
 	
 	ram_8_0 : ram_8x64
 		port map ( 
@@ -26,7 +28,7 @@ architecture systemc of fifo_64x8 is
 			addr => ram_addr, 
 			data_in => data_in, 
 			data_out => data_out, 
-			en => (rd OR wr),
+			en => ram_en,
 			we => wr
 		); 
 		
@@ -37,30 +39,34 @@ architecture systemc of fifo_64x8 is
 				wr_addr <= (others => '0');
 				rd_addr <= (others => '0');
 				byte_count <= (others => '0');
-				data_rdy <= '0' ;
+				ready <= '0' ;
 		 	elsif  clk'event and clk = '1' then
 				if  wr = '1'  and fullt = '0' then
 					ram_addr <= wr_addr ; 
 					wr_addr <= wr_addr + 1 ;
 					byte_count <= byte_count + 1 ;
+					ready <= '0' ;
 				elsif rd = '1' and wr = '0' and emptyt = '0' then
 					ram_addr <= rd_addr ; 
 					rd_addr <= rd_addr + 1 ;
 					byte_count <= byte_count - 1 ;
-					data_rdy <= '1' ;
+					ready <= '1' ;
 				else
-					data_rdy <= '0' ;
+					ready <= '0' ;
 				end if ;
 			end if;
 	end process;
 	
-	fullt <= '1' when byte_count = 80 else
-			  '0' ;
+	fullt <= '1' when byte_count = 63 else
+			   '0' ;
 	
 	emptyt <= '1' when byte_count = 0 else
-			   '0' ;
+			    '0' ;
 				
+	data_rdy <= ready ;	
 	full <= fullt ;
-	empty <= emptyt ;
+	
+	empty <= '1' when emptyt = '1' AND ready = '0' else
+				'0' ;
 	
 end systemc ;
