0e42b9d8925c7a5c7ac5a02a12c2995359466a5c
piat.jonathan <piat.jonathan@a556c50f-5b6a-0615-30e4-5cdc83f93d05>
2012-03-29 10:18:05 +0000

    improving viewer

--- viewer/Cam-preview/src/CamPreviewGUI.java
+++ viewer/Cam-preview/src/CamPreviewGUI.java
@@ -19,7 +19,9 @@ import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 class CamPreviewGUI extends JFrame implements Runnable, ActionListener {
 
@@ -33,13 +35,19 @@ class CamPreviewGUI extends JFrame implements Runnable, ActionListener {
 	PreviewPanel panel;
 	JComboBox comPortField;
 	JButton connectButton;
+	JButton sendConf;
 	SerialChannel serialChannel;
 	String comPort;
+	
+	JLabel dummy, upper, lower, yLabel, uLabel, vLabel ;
+	JTextField yu, yl, uu, ul, vu, vl ;
+	List<JTextField> registers ;
+	
 	ImageStreamParser parser;
 	List<String> ports;
 
 	public CamPreviewGUI() {
-		this.setPreferredSize(new Dimension(180, 190));
+		this.setPreferredSize(new Dimension(370, 190));
 		panel = new PreviewPanel();
 		panel.setSize(160, 120);
 		panel.setImage(Toolkit.getDefaultToolkit().getImage("jojo.jpeg"));
@@ -54,14 +62,68 @@ class CamPreviewGUI extends JFrame implements Runnable, ActionListener {
 		comPortField.setSize(320, 12);
 		connectButton = new JButton("connect");
 		connectButton.addActionListener(this);
+		
+		sendConf = new JButton("Send");
+		sendConf.addActionListener(this);
+		
 		this.setBackground(Color.WHITE);
 		this.setLayout(new BorderLayout(3, 3));
 		this.add("Center", panel);
+		
+		dummy = new JLabel();
+		upper = new JLabel("<=");
+		lower = new JLabel(">=");
+		yLabel = new JLabel("Y");
+		uLabel = new JLabel("U");
+		vLabel = new JLabel("V");
+		
+		registers = new ArrayList<JTextField>();
+
+		yu = new JTextField("55", 3);
+		registers.add(yu);
+		yl = new JTextField("00", 3);
+		registers.add(yl);
+		uu = new JTextField("55", 3);
+		registers.add(uu);
+		ul = new JTextField("00", 3);
+		registers.add(ul);
+		vu = new JTextField("55", 3);
+		registers.add(vu);
+		vl = new JTextField("00", 3);
+		registers.add(vl);
+		
+		JPanel confPanel = new JPanel();
+		confPanel.setLayout(new GridLayout(4, 3, 2, 2));
+		
+		confPanel.add(dummy);
+		confPanel.add(upper);
+		confPanel.add(lower);
+		
+		confPanel.add(yLabel);
+		confPanel.add(yu);
+		confPanel.add(yl);
+		
+		confPanel.add(uLabel);
+		confPanel.add(uu);
+		confPanel.add(ul);
+		
+		confPanel.add(vLabel);
+		confPanel.add(vu);
+		confPanel.add(vl);
+		
+		
+		
 		JPanel inputPanel = new JPanel();
 		inputPanel.setLayout(new GridLayout(1, 3, 10, 10));
+		
 		inputPanel.add(comPortField);
 		inputPanel.add(connectButton);
+		inputPanel.add(sendConf);
+		
 		this.add("South", inputPanel);
+		this.add("East", confPanel);
+		
+		
 		this.pack();
 		this.setVisible(true);
 		parser = new ImageStreamParser(this.panel);
@@ -115,7 +177,7 @@ class CamPreviewGUI extends JFrame implements Runnable, ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == connectButton || e.getSource() == comPortField) {
+		if (e.getSource() == connectButton) {
 			if (connectButton.getText().equals("connect")) {
 				if (serialChannel == null) {
 					try {
@@ -127,6 +189,28 @@ class CamPreviewGUI extends JFrame implements Runnable, ActionListener {
 					}
 					connectButton.setText("disconnect");
 				}
+			}
+			this.repaint();
+		}else if (e.getSource() == sendConf) {
+			if (serialChannel != null) {
+				try {
+					byte[] registerVals = new byte[6] ;
+					for(int i = 0; i < 6 ; i ++){
+						String val = registers.get(i).getText();
+						int iVal = Integer.decode(val);
+						if(iVal > 255){
+							iVal = 255 ;
+						}
+						registerVals[i] = (byte) iVal;
+						
+					}
+					serialChannel.writeDate(registerVals, 6);
+					
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				connectButton.setText("disconnect");
 			}
 			this.repaint();
 		}
