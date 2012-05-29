a29c37d3f96fd748f95b2d05d87eb00b6ed5c432
piat.jonathan <piat.jonathan@a556c50f-5b6a-0615-30e4-5cdc83f93d05>
2012-05-29 14:47:39 +0000

    update on lcd control. Will only support 320x240 @ 30hz . working fine !

---
+++ platform/papilio/SPARTCAM/spartcam_lcd_test.vhd
@@ -0,0 +1,182 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:16:17 02/19/2012 
+-- Design Name: 
+-- Module Name:    spartcam - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+library work;
+use work.camera.all ;
+use work.generic_components.all ;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity spartcam_lcd_test is
+port( CLK : in std_logic;
+		ARAZB	:	in std_logic;
+		TXD	:	out std_logic;
+		RXD   :	in std_logic;
+		
+		
+		--camera interface
+		CAM_XCLK	:	out std_logic;
+		CAM_SIOC, CAM_SIOD	:	inout std_logic; 
+		CAM_DATA	:	in std_logic_vector(7 downto 0);
+		CAM_PCLK, CAM_HREF, CAM_VSYNC	:	in std_logic;
+		CAM_RESET	:	out std_logic ;
+		
+		--LCD interface
+		LCD_RS, LCD_CS, LCD_WR, LCD_RD:	out std_logic;
+		LCD_DATA :	out std_logic_vector(15 downto 0);
+		
+		--FIFO interface
+		FIFO_CS, FIFO_WR, FIFO_RD, FIFO_A0:	out std_logic;
+		FIFO_DATA :	out std_logic_vector(7 downto 0)
+		
+);
+end spartcam_lcd_test;
+
+architecture Structural of spartcam_lcd_test is
+
+	COMPONENT dcm24
+	PORT(
+		CLKIN_IN : IN std_logic;          
+		CLKDV_OUT : OUT std_logic;
+		CLK0_OUT : OUT std_logic
+		);
+	END COMPONENT;
+
+	COMPONENT dcm96
+	PORT(
+		CLKIN_IN : IN std_logic;          
+		CLKFX_OUT : OUT std_logic;
+		CLKIN_IBUFG_OUT : OUT std_logic;
+		CLK0_OUT : OUT std_logic
+		);
+	END COMPONENT;
+	
+	component uart_tx is
+    port (            data_in : in std_logic_vector(7 downto 0);
+                 write_buffer : in std_logic;
+                 reset_buffer : in std_logic;
+                 en_16_x_baud : in std_logic;
+                   serial_out : out std_logic;
+                  buffer_full : out std_logic;
+             buffer_half_full : out std_logic;
+                          clk : in std_logic);
+    end component;
+
+	signal clk_24, clk_96, clk_48 : std_logic ;
+	signal baud_count, arazb_delayed, clk0 : std_logic ;
+	constant arazb_delay : integer := 1000000 ;
+	signal arazb_time : integer range 0 to 1048576 := arazb_delay ;
+
+	signal pixel_from_interface : std_logic_vector(7 downto 0);
+	signal pixel_from_ds : std_logic_vector(7 downto 0);
+	
+	signal pixel_from_conv : std_logic_vector(7 downto 0);
+
+	
+	signal data_to_send : std_logic_vector(7 downto 0);
+	signal send_signal, tx_buffer_full	:	std_logic ;
+	signal pxclk_from_interface, href_from_interface, vsync_from_interface : std_logic ;
+	signal pxclk_from_ds, href_from_ds, vsync_from_ds : std_logic ;
+	signal pxclk_from_conv, href_from_conv, vsync_from_conv : std_logic ;
+	
+	signal i2c_scl, i2c_sda : std_logic;
+	begin
+	
+	--comment connections below when using pins
+	--LCD_RS <= 'Z' ;
+	--LCD_CS <= 'Z' ; 
+	--LCD_WR <= 'Z' ; 
+	--LCD_RD <= 'Z' ;
+	--LCD_DATA <= (others => 'Z')  ;
+	FIFO_CS <= 'Z' ;
+	FIFO_WR <= 'Z' ; 
+	FIFO_RD <= 'Z' ; 
+	FIFO_A0 <= 'Z' ;
+	FIFO_DATA <= (others => 'Z')  ;
+	TXD <= 'Z' ;
+	
+	CAM_RESET <= arazb ;
+	CAM_XCLK <= clk_24 ;
+	
+	CAM_SIOC <= 'Z' ;
+	CAM_SIOD <= 'Z' ;
+
+	reset0: reset_generator 
+	generic map(HOLD_0 => 500000)
+	port map(clk => clk0, 
+		arazb => ARAZB ,
+		arazb_0 => arazb_delayed
+	 );
+	
+	process(clk_96) -- clk div for uart process
+	begin
+	if clk_96'event and clk_96 = '1' then
+		if baud_count = '1' then
+			baud_count <= '0' ;
+			clk_48 <= '1';
+		else
+			baud_count <= '1';
+			clk_48 <= '0';
+		end if;
+	end if;
+	end process;
+
+
+	Inst_dcm96: dcm96 PORT MAP(
+		CLKIN_IN => clk,
+		CLKFX_OUT => clk_96, 
+		CLKIN_IBUFG_OUT => clk0
+	);	
+
+
+	Inst_dcm24: dcm24 PORT MAP(
+		CLKIN_IN => clk_96,
+		CLKDV_OUT => clk_24
+	);
+	
+	
+		graphic_gen : graphic_generator 
+		port map(clk => clk_24, arazb => arazb,
+			  pixel_clock_out => pxclk_from_interface, hsync_out => href_from_interface, vsync_out => vsync_from_interface ,
+			  pixel_r => pixel_from_interface
+	   );
+		
+		lcd_controller0 : lcd_controller 
+		port map(
+				clk => clk_96,
+				arazb => arazb_delayed, 
+				pixel_clock => pxclk_from_interface, hsync => href_from_interface, vsync => vsync_from_interface, 
+				pixel_r => pixel_from_interface, pixel_g =>  X"00", pixel_b =>  X"00",
+				lcd_rs => LCD_RS, lcd_cs => LCD_CS, lcd_rd => LCD_RD, lcd_wr => LCD_WR,
+				lcd_data	=> LCD_DATA
+			); 
+
+
+end Structural;
+
