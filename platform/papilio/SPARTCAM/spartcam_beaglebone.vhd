8bc7a895ea05618a0e78f273dbae893c5a2ad56e
piat.jonathan <piat.jonathan@a556c50f-5b6a-0615-30e4-5cdc83f93d05>
2012-06-21 15:36:29 +0000

    designing fifo to be interface with cpu. Should work at frequency less than system fre (96mhz)

---
+++ platform/papilio/SPARTCAM/spartcam_beaglebone.vhd
@@ -0,0 +1,198 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:14:22 06/21/2012 
+-- Design Name: 
+-- Module Name:    spartcam_beaglebone - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+library work ;
+use work.generic_components.all ;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity spartcam_beaglebone is
+port( CLK : in std_logic;
+		ARAZB	:	in std_logic;
+		TXD	:	out std_logic;
+		RXD   :	in std_logic;
+		
+		
+		--camera interface
+		CAM_XCLK	:	out std_logic;
+		CAM_SIOC, CAM_SIOD	:	inout std_logic; 
+		CAM_DATA	:	in std_logic_vector(7 downto 0);
+		CAM_PCLK, CAM_HREF, CAM_VSYNC	:	in std_logic;
+		CAM_RESET	:	out std_logic ;
+		
+		--LCD interface
+		BEAGLE_ALE, BEAGLE_CSN, BEAGLE_WRN, BEAGLE_OEN:	in std_logic;
+		BEAGLE_DATA :	inout std_logic_vector(15 downto 0);
+		
+		--FIFO interface
+		FIFO_CS, FIFO_WR, FIFO_RD, FIFO_A0:	out std_logic;
+		FIFO_DATA :	out std_logic_vector(7 downto 0)
+		
+);
+end spartcam_beaglebone;
+
+architecture Behavioral of spartcam_beaglebone is
+
+	COMPONENT dcm24
+	PORT(
+		CLKIN_IN : IN std_logic;          
+		CLKDV_OUT : OUT std_logic;
+		CLK0_OUT : OUT std_logic
+		);
+	END COMPONENT;
+
+	COMPONENT dcm96
+	PORT(
+		CLKIN_IN : IN std_logic;          
+		CLKFX_OUT : OUT std_logic;
+		CLKIN_IBUFG_OUT : OUT std_logic;
+		CLK0_OUT : OUT std_logic
+		);
+	END COMPONENT;
+
+	component uart_tx is
+    port (            data_in : in std_logic_vector(7 downto 0);
+                 write_buffer : in std_logic;
+                 reset_buffer : in std_logic;
+                 en_16_x_baud : in std_logic;
+                   serial_out : out std_logic;
+                  buffer_full : out std_logic;
+             buffer_half_full : out std_logic;
+                          clk : in std_logic);
+    end component;
+	 
+	 
+	 component uart_rx is
+    port (            serial_in : in std_logic;
+                       data_out : out std_logic_vector(7 downto 0);
+                    read_buffer : in std_logic;
+                   reset_buffer : in std_logic;
+                   en_16_x_baud : in std_logic;
+            buffer_data_present : out std_logic;
+                    buffer_full : out std_logic;
+               buffer_half_full : out std_logic;
+                            clk : in std_logic);
+    end component;
+signal clk_24, clk_96, clk_48, clk_1_8 : std_logic ;
+	signal baud_count, arazb_delayed, clk0, cam_reset_delayed : std_logic ;
+	signal baud_rate_divider : integer range 0 to 53 := 0 ;
+	signal data_to_send : std_logic_vector(7 downto 0);
+	signal data_to_read : std_logic_vector(7 downto 0);
+	signal send_signal, tx_buffer_full, read_signal, data_present	:	std_logic ;
+	
+	signal mem_ad, bus_addr, bus_data : std_logic_vector(15 downto 0);
+	signal bus_wr, bus_rd : std_logic ;
+begin
+
+	reset0: reset_generator 
+	generic map(HOLD_0 => 500000)
+	port map(clk => clk0, 
+		arazb => ARAZB ,
+		arazb_0 => arazb_delayed
+	 );
+
+	Inst_dcm96: dcm96 PORT MAP(
+		CLKIN_IN => clk,
+		CLKFX_OUT => clk_96, 
+		CLKIN_IBUFG_OUT => clk0
+	);	
+
+
+	Inst_dcm24: dcm24 PORT MAP(
+		CLKIN_IN => clk_96,
+		CLKDV_OUT => clk_24
+	);
+
+
+	process(clk_96) -- clk div for uart 3Mbs process
+	begin
+	if clk_96'event and clk_96 = '1' then
+		if baud_count = '1' then
+			baud_count <= '0' ;
+			clk_48 <= '1';
+		else
+			baud_count <= '1';
+			clk_48 <= '0';
+		end if;
+	end if;
+	end process;
+
+
+
+
+mem_interface0 : muxed_addr_interface
+generic map(ADDR_WIDTH => 16 , DATA_WIDTH => 16)
+port map(clk => clk_96 , arazb => arazb_delayed ,
+	  data	=> BEAGLE_DATA ,
+	  wrn => BEAGLE_WRN, oen => BEAGLE_OEN, addr_en_n => BEAGLE_ALE,  csn => BEAGLE_CSN ,
+	  data_bus	=> bus_data ,
+	  addr_bus	=> bus_addr ,
+	  wr => bus_wr, rd => bus_rd
+);
+
+
+ bi_fifo0 : fifo_peripheral 
+			generic map(BASE_ADDR => 0, WIDTH => 16, SIZE => 128)
+			port map(
+          clk => clk_96,
+          arazb => arazb_delayed,
+          addr_bus => bus_addr,
+          wr_bus => bus_wr,
+          rd_bus => bus_rd,
+          wrB => ((NOT fullA) AND data_present),
+          rdA => NOT emptyA,
+          data_bus => bus_data,
+          inputB => data_to_read,
+          outputA => data_to_send,
+          emptyA => emptyA,
+          fullA => fullA,
+          emptyB => emptyB,
+          fullB => fullB
+        );
+
+	uart_tx0 : uart_tx 
+    port map (   data_in => data_to_send, 
+                 write_buffer => NOT emptyA,
+                 reset_buffer => NOT arazb_delayed, 
+                 en_16_x_baud => clk_48,
+                 serial_out => TXD,
+                 clk => clk_96);
+
+	uart_rx0 : uart_rx 
+    port map(            serial_in => RXD,
+                       data_out => data_to_read,
+                    read_buffer => ((NOT fullA) AND data_present),
+                   reset_buffer => NOT arazb_delayed,
+                   en_16_x_baud => clk_48,
+            buffer_data_present => data_present,
+                            clk => clk_96);
+
+
+end Behavioral;
+
