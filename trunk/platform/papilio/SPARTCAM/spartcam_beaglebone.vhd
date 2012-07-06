5eb1f40bf679260a715d18f4e1abefd4b9a4b5b8
piat.jonathan <piat.jonathan@a556c50f-5b6a-0615-30e4-5cdc83f93d05>
2012-07-06 15:37:09 +0000

    improving fifo example with gpmc of beaglebone

--- trunk/platform/papilio/SPARTCAM/spartcam_beaglebone.vhd
+++ trunk/platform/papilio/SPARTCAM/spartcam_beaglebone.vhd
@@ -26,6 +26,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 library work ;
 use work.generic_components.all ;
+use work.camera.all ;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -59,7 +60,7 @@ end spartcam_beaglebone;
 architecture Behavioral of spartcam_beaglebone is
 
 
-	COMPONENT dcm120
+	COMPONENT dcm_32_120
 	PORT(
 		CLKIN_IN : IN std_logic;          
 		CLKFX_OUT : OUT std_logic;
@@ -68,157 +69,124 @@ architecture Behavioral of spartcam_beaglebone is
 		LOCKED_OUT : OUT std_logic
 		);
 	END COMPONENT;
+	
+	COMPONENT dcm_120_24
+	PORT(
+		CLKIN_IN : IN std_logic;          
+		CLKDV_OUT : OUT std_logic;
+		CLK0_OUT : OUT std_logic;
+		LOCKED_OUT : OUT std_logic
+		);
+	END COMPONENT;
 
-
-	component uart_tx is
-    port (            data_in : in std_logic_vector(7 downto 0);
-                 write_buffer : in std_logic;
-                 reset_buffer : in std_logic;
-                 en_16_x_baud : in std_logic;
-                   serial_out : out std_logic;
-                  buffer_full : out std_logic;
-             buffer_half_full : out std_logic;
-                          clk : in std_logic);
-    end component;
-	 
-	 
-	 component uart_rx is
-    port (            serial_in : in std_logic;
-                       data_out : out std_logic_vector(7 downto 0);
-                    read_buffer : in std_logic;
-                   reset_buffer : in std_logic;
-                   en_16_x_baud : in std_logic;
-            buffer_data_present : out std_logic;
-                    buffer_full : out std_logic;
-               buffer_half_full : out std_logic;
-                            clk : in std_logic);
-    end component;
-signal clk_120, clk_1_8 : std_logic ;
+signal clk_120, clk_24: std_logic ;
 	signal baud_count, arazb_delayed, clk0, clk0_buf, cam_reset_delayed : std_logic ;
 	signal baud_rate_divider : integer range 0 to 53 := 0 ;
-	signal data_to_send, fifo_output : std_logic_vector(15 downto 0);
-	signal data_to_read, fifo_input : std_logic_vector(15 downto 0);
-	signal send_signal, send_addr, send_data,tx_buffer_full, read_signal, data_present	:	std_logic ;
+	signal cam_clock_divider : integer range 0 to 10 := 0 ;
+	
+	
+	signal fifo_output : std_logic_vector(15 downto 0);
+	signal fifo_input : std_logic_vector(15 downto 0);
+	signal fifoB_wr, fifoA_rd, fifoA_empty, fifoA_full, fifoB_empty, fifoB_full : std_logic ;
 	
 	signal bus_data_in, bus_data_out : std_logic_vector(15 downto 0);
 	signal bus_addr : std_logic_vector(7 downto 0);
 	signal bus_wr, bus_rd, bus_cs : std_logic ;
-	signal fifoB_wr, fifoA_rd, fifoA_empty, fifoA_full, fifoB_empty, fifoB_full : std_logic ;
+	
+	signal pixel_from_interface : std_logic_vector(7 downto 0);
+	signal pxclk_from_interface, href_from_interface, vsync_from_interface : std_logic ;
+
 begin
 
 --comment connections below when using pins
 	FIFO_CS <= bus_cs ;
-	FIFO_WR <= bus_wr ; 
-	FIFO_RD <= bus_rd ; 
+	FIFO_WR <= href_from_interface ; 
+	FIFO_RD <= pxclk_from_interface ; 
 	FIFO_A0 <= 'Z' ;
 	
-	CAM_RESET <= 'Z' ;
-	CAM_XCLK <= 'Z' ;
---		CAM_PCLK, CAM_HREF, CAM_VSYNC	:	in std_logic;
-	
-	CAM_SIOC <= 'Z' ;
-	CAM_SIOD <= 'Z' ;
+	CAM_RESET <= ARAZB ;
 
 	TXD <= 'Z' ;
 
 
 	reset0: reset_generator 
 	generic map(HOLD_0 => 500000)
-	port map(clk => clk0_buf, 
+	port map(clk => clk0, 
 		arazb => ARAZB ,
 		arazb_0 => arazb_delayed
 	 );
 
 
 	
-	Inst_dcm120: dcm120 PORT MAP(
+	Inst_dcm_32_120: dcm_32_120 PORT MAP(
 		CLKIN_IN => clk,
 		CLKFX_OUT => clk_120,
-		CLKIN_IBUFG_OUT => clk0_buf
+		CLK0_OUT => clk0
 	);	
+	
+	Inst_dcm_120_24: dcm_120_24 PORT MAP(
+		CLKIN_IN => clk_120,
+		CLKDV_OUT => clk_24
+	);
 
 
 
-	process(clk_120) -- div for 15200kbps
-			begin
-			if clk_120'event and clk_120='1' then
-				if baud_rate_divider=65 then
-					baud_rate_divider <= 0;
-					clk_1_8 <= '1';
-				else
-					baud_rate_divider <= baud_rate_divider + 1;
-					clk_1_8 <= '0';
-				end if;
-			end if;
-	end process;
 
-
---mem_interface0 : muxed_addr_interface
---generic map(ADDR_WIDTH => 8 , DATA_WIDTH => 16)
---port map(clk => clk_120 , arazb => arazb_delayed ,
---	  data	=> BEAGLE_DATA ,
---	  wrn => BEAGLE_WRN, oen => BEAGLE_OEN, addr_en_n => BEAGLE_ALE,  csn => BEAGLE_CSN ,
---	  data_bus_in	=> bus_data_in ,
---	  data_bus_out	=> bus_data_out ,
---	  addr_bus	=> bus_addr ,
---	  wr => bus_wr, rd => bus_rd
---);
 
 mem_interface0 : addr_interface
 generic map(ADDR_WIDTH => 8 , DATA_WIDTH => 16)
-port map(clk => clk_120 , arazb => arazb_delayed ,
-	  data	=> BEAGLE_DATA ,
-	  addr	=> BEAGLE_ADDR ,
-	  wrn => BEAGLE_WRN, oen => BEAGLE_OEN, csn => BEAGLE_CSN ,
-	  data_bus_in	=> bus_data_in ,
-	  data_bus_out	=> bus_data_out ,
-	  addr_bus	=> bus_addr ,
-	  wr => bus_wr, rd => bus_rd
-);
+		port map(clk => clk_120 , arazb => arazb_delayed ,
+		  data	=> BEAGLE_DATA ,
+		  addr	=> BEAGLE_ADDR ,
+		  wrn => BEAGLE_WRN, oen => BEAGLE_OEN, csn => BEAGLE_CSN ,
+		  data_bus_in	=> bus_data_in ,
+		  data_bus_out	=> bus_data_out ,
+		  addr_bus	=> bus_addr ,
+		  wr => bus_wr, rd => bus_rd
+		);
 
 
 
+bi_fifo0 : fifo_peripheral 
+		generic map(BASE_ADDR => 0, ADDR_WIDTH => 8,WIDTH => 16, SIZE => 1024)
+		port map(
+			clk => clk_120,
+			arazb => arazb_delayed,
+			addr_bus => bus_addr,
+			wr_bus => bus_wr,
+			rd_bus => bus_rd,
+			wrB => fifoB_wr,
+			rdA => fifoA_rd,
+			data_bus_in => bus_data_out,
+			data_bus_out => bus_data_in,
+			inputB => fifo_input,
+			outputA => fifo_output,
+			emptyA => fifoA_empty,
+			fullA => fifoA_full,
+			emptyB => fifoB_empty,
+			fullB => fifoB_full
+		);
+	
+ camera0: yuv_camera_interface
+		generic map(FORMAT => QVGA)
+		port map(clock => clk_120,
+			pixel_data => CAM_DATA, 
+			i2c_clk => clk_24,
+			scl => CAM_SIOC ,
+			sda => CAM_SIOD ,
+			arazb => arazb_delayed,
+			pxclk => CAM_PCLK, href => CAM_HREF, vsync => CAM_VSYNC,
+			pixel_clock_out => pxclk_from_interface, hsync_out => href_from_interface, vsync_out => vsync_from_interface,
+			y_data => pixel_from_interface
+		);	
+
+CAM_XCLK <= clk_24 ;
 
 
+fifoB_wr <= pxclk_from_interface OR vsync_from_interface;
+fifo_input <= (X"AA55") when vsync_from_interface = '1' else
+				  (X"00" & pixel_from_interface);
 
- bi_fifo0 : fifo_peripheral 
-			generic map(BASE_ADDR => 0, ADDR_WIDTH => 8,WIDTH => 16, SIZE => 512)
-			port map(
-          clk => clk_120,
-          arazb => arazb_delayed,
-          addr_bus => bus_addr,
-          wr_bus => bus_wr,
-          rd_bus => bus_rd,
-          wrB => fifoB_wr,
-          rdA => fifoA_rd,
-          data_bus_in => bus_data_out,
-			 data_bus_out => bus_data_in,
-          inputB => fifo_output,
-          outputA => fifo_output,
-          emptyA => fifoA_empty,
-          fullA => fifoA_full,
-          emptyB => fifoB_empty,
-          fullB => fifoB_full
-        );
-		  
---fifoB_wr <= '0' ;
---fifoA_rd <= '0' ;
-
-process(clk_120, arazb)
-begin
-if arazb_delayed = '0' then
-		fifoB_wr <= '0' ;
-		fifoA_rd <= '0' ;
-elsif clk_120'event and clk_120 ='1' then
-	if fifoA_empty = '0' then
-		fifoB_wr <= '1' ;
-		fifoA_rd <= '1' ;
-	else
-		fifoB_wr <= '0' ;
-		fifoA_rd <= '0' ;
-	end if ;
-end if ;
-end process ;
 
 end Behavioral;
 
