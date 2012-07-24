46fcb28a75269ceacc1d09c552928ab43add0834
piat.jonathan <piat.jonathan@a556c50f-5b6a-0615-30e4-5cdc83f93d05>
2012-07-24 08:18:03 +0000

    improving fifo example with gpmc of beaglebone

--- beaglebone/test_gpmc.c
+++ beaglebone/test_gpmc.c
@@ -8,69 +8,88 @@
 #include <sys/mman.h>
 #include <errno.h>
 #include <time.h>
+#include <linux/ioctl.h>
 
 
-#define MEM_BASE_ADDR	 0x09000000
 
+/* Use 'p' as magic number */
+#define PAPILIO_FIFO_IOC_MAGIC 'p'
+/* Please use a different 8-bit number in your code */
+#define PAPILIO_FIFO_RESET _IO(PAPILIO_FIFO_IOC_MAGIC, 0)
+
+
+#define PAPILIO_FIFO_PEEK _IOR(PAPILIO_FIFO_IOC_MAGIC, 1, short)
+#define PAPILIO_FIFO_NB_FREE _IOR(PAPILIO_FIFO_IOC_MAGIC, 2, short)
+#define PAPILIO_FIFO_NB_AVAILABLE _IOR(PAPILIO_FIFO_IOC_MAGIC, 3, short)
+#define PAPILIO_FIFO_MODE _IO(PAPILIO_FIFO_IOC_MAGIC, 4)
+#define PAPILIO_DIRECT_MODE _IO(PAPILIO_FIFO_IOC_MAGIC, 5)
+
+
+#define TRANSFER_SIZE	1024
 
 int main(int argc, char ** argv){
 	int fd ;
 	long start_time, end_time ;
 	double diff_time ;
 	struct timespec cpu_time ;
-	int i, page_size ;
-	volatile unsigned short * gpmc_pointer ; 
+	int i, page_size, transfer_count ; 
 
-	unsigned short dummy_data [100] ;
-	for(i = 0 ; i < 100 ; i ++){
+	unsigned char dummy_data [TRANSFER_SIZE] ;
+	for(i = 0 ; i < TRANSFER_SIZE ; i ++){
 		dummy_data[i] = i ;
 	}
 
-	fd = open("/dev/mem", O_RDWR | O_SYNC);
+	fd = open("/dev/papilio0", O_RDWR | O_SYNC);
 	if(fd == -1){
 		perror("Error opening file");
-		printf("error opening /dev/mem \n");
+		printf("error opening /dev/papilio0 \n");
 		exit(EXIT_FAILURE);
 	}
-
-
-	page_size = getpagesize();
-	printf("page size is : %d \n", page_size);
-	//sleep(1);
-	gpmc_pointer = (volatile unsigned short *) mmap(0, page_size, PROT_READ | PROT_WRITE, 
-	MAP_SHARED ,fd, 
-	MEM_BASE_ADDR);
-	if(gpmc_pointer == -1){
-		printf("cannot allocate pointer on %x \n", MEM_BASE_ADDR);
-		exit(EXIT_FAILURE);
-	}
-	printf("pointer allocated with address %x \n", gpmc_pointer);
-	printf("nb free in fifo %d \n", gpmc_pointer[1]);
-	printf("writing 100 short int \n");
+	
+	printf("testing fifo mode  \n");
+	ioctl(fd, PAPILIO_FIFO_MODE) ;
+	printf("issuing reset to fifo \n");
+	ioctl(fd, PAPILIO_FIFO_RESET);
+	printf("fifo has nb %d free places \n", ioctl(fd, PAPILIO_FIFO_NB_FREE));
+	printf("writing %d bytes \n", TRANSFER_SIZE);
 	clock_gettime(CLOCK_REALTIME, &cpu_time);
 	start_time = cpu_time.tv_nsec ;
-	//memcpy((void *) gpmc_pointer, (void *) dummy_data, 100 * sizeof(unsigned short));
-	for(i = 0 ; i < 100 ; i++){
-		gpmc_pointer[0] = i ;
-	}
+	transfer_count = write(fd, dummy_data, TRANSFER_SIZE);
 	clock_gettime(CLOCK_REALTIME, &cpu_time);
 	end_time = cpu_time.tv_nsec ;
 	diff_time = end_time - start_time ;
 	diff_time = diff_time/1000000000 ;
-	printf("transfer_started at %d, ended at %d \n", start_time, 
-end_time);
-	printf("transffered %d bytes in %f s : %f B/s \n", 
-100*sizeof(unsigned short), diff_time, (100 * sizeof(unsigned 
-short))/diff_time);
-	printf("nb free in fifo  %d \n", gpmc_pointer[1]);
-	printf("nb available in fifo	%d \n", gpmc_pointer[2]);
-	printf("reading from fifo : ");
-	for( i = 0 ; i < 100 ; i ++ ){
-		printf("%d, ", gpmc_pointer[0]);	
-	}
-	printf("\n");	
-	printf("nb available in fifo %d \n", gpmc_pointer[2]);
-	munmap((void *) gpmc_pointer, page_size);
+	printf("transfer_started at %d, ended at %d \n", start_time, end_time);
+	printf("transffered %d bytes in %f s : %f B/s \n", transfer_count, diff_time, (transfer_count)/diff_time);
+	printf("fifo has nb %d free places \n", ioctl(fd, PAPILIO_FIFO_NB_FREE));
+
+	printf("fifo has nb %d available places \n", ioctl(fd, PAPILIO_FIFO_NB_AVAILABLE));
+
+	printf("reading  %d bytes \n", TRANSFER_SIZE);
+	clock_gettime(CLOCK_REALTIME, &cpu_time);
+	start_time = cpu_time.tv_nsec ;
+	transfer_count = read(fd, dummy_data, TRANSFER_SIZE);
+	clock_gettime(CLOCK_REALTIME, &cpu_time);
+	end_time = cpu_time.tv_nsec ;
+	diff_time = end_time - start_time ;
+	diff_time = diff_time/1000000000 ;
+	printf("transfer_started at %d, ended at %d \n", start_time, end_time);
+	printf("transffered %d bytes in %f s : %f B/s \n", transfer_count, diff_time, (transfer_count)/diff_time);
+	printf("fifo has nb %d available places \n ",ioctl(fd, PAPILIO_FIFO_NB_AVAILABLE) );	
+
+	printf("testing direct mode  \n");
+	ioctl(fd, PAPILIO_DIRECT_MODE) ;
+	
+	clock_gettime(CLOCK_REALTIME, &cpu_time);
+	start_time = cpu_time.tv_nsec ;
+	transfer_count = read(fd, dummy_data, TRANSFER_SIZE);
+	clock_gettime(CLOCK_REALTIME, &cpu_time);
+	end_time = cpu_time.tv_nsec ;
+	diff_time = end_time - start_time ;
+	diff_time = diff_time/1000000000 ;
+	printf("transfer_started at %d, ended at %d \n", start_time, end_time);
+	printf("transffered %d bytes in %f s : %f B/s \n", transfer_count, diff_time, (transfer_count)/diff_time);
+
 	close(fd);
 	return 0 ;
 }
