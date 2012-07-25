9a701782702c502f808fe8585ab0ff16b1622f0c
piat.jonathan <piat.jonathan@a556c50f-5b6a-0615-30e4-5cdc83f93d05>
2012-07-25 14:28:31 +0000

    improving fifo example with gpmc of beaglebone

--- hdl/addr_interface.vhd
+++ hdl/addr_interface.vhd
@@ -19,6 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -43,9 +44,13 @@ port(clk, arazb : in std_logic ;
 end addr_interface;
 
 architecture Behavioral of addr_interface is
-signal wrt, rdt, cst, nclk : std_logic ;
+signal wr_from_bus , rd_from_bus, wrt, rdt, cst, nclk, latch_en, old_oen : std_logic ;
 signal data_bus_out_t, data_bus_in_t	: std_logic_vector((DATA_WIDTH - 1) downto 0);
+signal debounce_counter : std_logic_vector(3 downto 0)  ;
 begin
+
+wr_from_bus <= (NOT wrn) and (NOT csn) ;
+rd_from_bus <= (NOT oen) and (NOT csn) ;
 
 
 gen_ext_clk : if USE_EXT_CLOCK generate
@@ -58,8 +63,8 @@ gen_ext_clk : if USE_EXT_CLOCK generate
 		addr_bus <= (others => '0') ;
 		data_bus_in_t <= (others => '0') ;
 	elsif ext_clk'event and ext_clk ='1' then
-		wrt <= (NOT wrn) and (NOT csn)  ;
-		rdt <= (NOT oen) and (NOT csn)  ;
+		wrt <= wr_from_bus  ;
+		rdt <= rd_from_bus  ;
 		addr_bus <= addr ;
 		data_bus_out_t <= data ;
 		data_bus_in_t <= data_bus_in ;
@@ -77,10 +82,12 @@ gen_no_ext_clk : if (NOT USE_EXT_CLOCK) generate
 		addr_bus <= (others => '0') ;
 		--data_bus_in_t <= (others => '0') ;
 	elsif clk'event and clk ='1' then
-		wrt <= (NOT wrn) and (NOT csn) ;
-		rdt <= (NOT oen) and (NOT csn) ;
-		addr_bus <= addr ;
-		data_bus_out_t <= data ;
+		if latch_en = '1' then
+			wrt <= wr_from_bus  ;
+			rdt <= rd_from_bus  ;
+			addr_bus <= addr ;
+			data_bus_out_t <= data ;
+		end if ;
 		--data_bus_in_t <= data_bus_in ;
 	end if ;
 	end process;
@@ -108,11 +115,41 @@ gen_no_ext_clk : if (NOT USE_EXT_CLOCK) generate
 end generate ;
 
 
+--latch_en <= '1' ;
+--debounce 
+--rs lock
+process(clk, arazb)
+begin
+	if arazb = '0' then
+		latch_en <= '1' ;
+	elsif clk'event and clk = '1' then
+		if wr_from_bus = '1' or rd_from_bus = '1' then
+			latch_en <= '0' ;
+		elsif debounce_counter = 1 then
+			latch_en <= '1' ;
+		end if ;
+	end if ;
+end process ;
+
+process(clk, arazb)
+begin
+	if arazb = '0' then
+		debounce_counter <= (others => '0') ;
+	elsif clk'event and clk = '1' then
+		if debounce_counter = 1 then
+			debounce_counter <= (others => '0') ;
+		elsif latch_en = '0' then
+			debounce_counter <=  debounce_counter + 1;
+		end if ;
+	end if ;
+end process ;
+
+--end debounce
 
 wr <= wrt ;
 rd <= rdt ;
 
-data <= data_bus_in_t when oen='0' and csn='0' else--rdt = '1' else
+data <= data_bus_in_t when rdt = '1' else
 		  (others => 'Z');
 
 data_bus_out <= data_bus_out_t ;
