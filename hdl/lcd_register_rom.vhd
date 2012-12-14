dd0996f7eca230d7410fa1b369950fdb18a6b7ca
piat.jonathan <piat.jonathan@gmail.com>
2012-12-14 14:25:41 +0000

    added configuration for ssd1289 based LCD

--- hdl/lcd_register_rom.vhd
+++ hdl/lcd_register_rom.vhd
@@ -96,3 +96,89 @@ architecture lcd2_4 of lcd_register_rom is
 		 end process;  
 	
 end lcd2_4 ;
+
+
+
+architecture lcd3_2 of lcd_register_rom is
+ 
+        type array_61 is array (0 to 60) of std_logic_vector(23 downto 0 ); 
+        
+        -- CONFIGURATION TAKEN FROM ARDUINO LIBRARY
+        signal rom : array_61 :=( 
+        (x"00" & x"0001"),
+        (x"03" & x"A8A4"),
+        (x"0C" & x"0000"),
+        (x"0D" & x"8007"),
+        (x"0E" & x"2B00"),
+        (x"1E" & x"00B7"),
+        (x"01" & x"2B3F"),
+        (x"02" & x"0600"),
+        (x"10" & x"0000"),
+        (x"11" & x"6058"),
+        (x"05" & x"0000"),
+        (x"06" & x"0000"),
+        (x"16" & x"EF1C"),
+        (x"17" & x"0003"),
+        (x"07" & x"0233"),
+        (x"0B" & x"0000"),
+        (x"0F" & x"0000"),
+        (x"41" & x"0000"),
+        (x"42" & x"0000"),
+        (x"48" & x"0000"),
+        (x"49" & x"013F"),
+        (x"4A" & x"0000"),
+        (x"4B" & x"0000"),
+        (x"44" & x"EF00"),
+        (x"45" & x"0000"),
+        (x"46" & x"013F"),
+        (x"30" & x"0707"),
+        (x"31" & x"0204"),
+        (x"32" & x"0204"),
+        (x"33" & x"0502"),
+        (x"34" & x"0507"),
+        (x"35" & x"0204"),
+        (x"36" & x"0204"),
+        (x"37" & x"0502"),
+        (x"3A" & x"0302"),
+        (x"3B" & x"0302"),
+        (x"23" & x"0000"),
+        (x"24" & x"0000"),
+        (x"25" & x"8000"),
+        (x"28" & x"0006"),
+        (x"2f" & x"12BE"),
+        (x"12" & x"6CEB"),
+        (x"4f" & x"0000"),
+        (x"4e" & x"0000"),
+        (x"ffffff"),
+        (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff"),
+                  (x"ffffff")
+);
+
+        begin
+        
+        
+        -- rom_process
+        process(clk)
+                 begin
+                 if clk'event and clk = '1' then
+                        if en = '1' then
+                                data <= rom(conv_integer(addr)) ;
+                        end if;
+                 end if;
+                 end process;  
+        
+end lcd3_2 ;
