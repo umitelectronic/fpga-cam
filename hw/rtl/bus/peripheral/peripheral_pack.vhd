c0da140bc5007d73cd0328872ed119c7f94c1802
piat.jonathan <piat.jonathan@gmail.com>
2012-12-18 14:13:58 +0000

    small changes to allow multiple peripheral on one bus

--- hw/rtl/bus/peripheral/peripheral_pack.vhd
+++ hw/rtl/bus/peripheral/peripheral_pack.vhd
@@ -13,17 +13,30 @@ use IEEE.STD_LOGIC_1164.all;
 package peripheral_pack is
 
 component fifo_peripheral is
-generic(BASE_ADDR	:	natural	:= 0; ADDR_WIDTH : positive := 8; WIDTH	: positive := 16; SIZE	: positive	:= 128);
+generic(ADDR_WIDTH : positive := 8; WIDTH	: positive := 16; SIZE	: positive	:= 128);
 port(
 	clk, resetn : in std_logic ;
 	addr_bus : in std_logic_vector((ADDR_WIDTH - 1) downto 0);
-	wr_bus, rd_bus : in std_logic ;
+	wr_bus, rd_bus, cs_bus : in std_logic ;
 	wrB, rdA : in std_logic ;
 	data_bus_in	: in std_logic_vector((WIDTH - 1) downto 0); -- bus interface
 	data_bus_out	: out std_logic_vector((WIDTH - 1) downto 0); -- bus interface
 	inputB: in std_logic_vector((WIDTH - 1) downto 0); -- logic interface
 	outputA	: out std_logic_vector((WIDTH - 1) downto 0); -- logic interface
 	emptyA, fullA, emptyB, fullB	:	out std_logic 
+);
+end component;
+
+component latch_peripheral is
+generic(ADDR_WIDTH : positive := 8; WIDTH	: positive := 16);
+port(
+	clk, resetn : in std_logic ;
+	addr_bus : in std_logic_vector((ADDR_WIDTH - 1) downto 0);
+	wr_bus, rd_bus, cs_bus : in std_logic ;
+	data_bus_in	: in std_logic_vector((WIDTH - 1) downto 0); -- bus interface
+	data_bus_out	: out std_logic_vector((WIDTH - 1) downto 0); -- bus interface
+	latch_input : in  std_logic_vector((WIDTH - 1) downto 0);
+	latch_output :out  std_logic_vector((WIDTH - 1) downto 0)
 );
 end component;
 
